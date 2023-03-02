abc3878018308641fee688ad0ebf68a61d05da12
Kilian Gandolf <116080187+KilianG98@users.noreply.github.com>
2023-03-02 16:33:34 +0100

    Update KilianG98-HelloWorld.py

--- A0/KilianG98-HelloWorld.py
+++ A0/KilianG98-HelloWorld.py
@@ -1,7 +1,7 @@
 if __name__=="__main__":
     
     inFileName = input("pls enter the input filename:\n")
-    print("Hello World!")
+    print("Hello World!\n")
     with open (inFileName, "r") as f:
         for line in f:
             print(line)
