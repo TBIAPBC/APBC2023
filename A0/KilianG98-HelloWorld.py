647a272feed2a870e42a91224901b598fc708799
KilianG98 <kilian1gandolf@gmail.com>
2023-03-06 11:22:20 +0100

    final

--- A0/KilianG98-HelloWorld.py
+++ A0/KilianG98-HelloWorld.py
@@ -1,8 +1,8 @@
 if __name__=="__main__":
     
     inFileName = input("pls enter the input filename:\n")
-    print("Hello World!\n")
     with open (inFileName, "r") as f:
-        for line in f:
-            print(line)
+        output = f.read()
+    print("Hello World!\n",output, sep ='', end='')
+    
     
