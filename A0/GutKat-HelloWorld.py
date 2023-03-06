f707cb94f5482ca78af185414b337bece5cb2dcc
GutKat <119005289+GutKat@users.noreply.github.com>
2023-03-06 09:50:52 +0100

    Update GutKat-HelloWorld.py

--- A0/GutKat-HelloWorld.py
+++ A0/GutKat-HelloWorld.py
@@ -5,7 +5,6 @@ parser = argparse.ArgumentParser(description='A0 - Warm up assignment: Hello Wor
 parser.add_argument('-i', '--input', type=str, help='the input file')
 args = parser.parse_args()
 
-output =os.path.splitext(args.input)[0] + ".out"
 with open(args.input, "r") as f:
     content = f.read()
 
