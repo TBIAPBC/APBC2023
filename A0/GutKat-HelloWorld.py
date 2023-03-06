1d96fa463f6ee525aeba14582ca179336c3f67e3
GutKat <katrin.gutenbrunne@gmail.com>
2023-03-06 09:49:47 +0100

    update GutKat-HelloWorld.py

--- A0/GutKat-HelloWorld.py
+++ A0/GutKat-HelloWorld.py
@@ -6,10 +6,8 @@ parser.add_argument('-i', '--input', type=str, help='the input file')
 args = parser.parse_args()
 
 output =os.path.splitext(args.input)[0] + ".out"
-print(args.input, output)
 with open(args.input, "r") as f:
     content = f.read()
 
-with open(output, "w") as f:
-    f.write("Hello World! \n")
-    f.write(content)
+print("Hello World!")
+print(content)
