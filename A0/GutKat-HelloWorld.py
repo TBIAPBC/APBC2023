0255709db6bdfedf0f59d53cc530abfeca36b9ef
GutKat <119005289+GutKat@users.noreply.github.com>
2023-03-07 15:14:24 +0100

    Update GutKat-HelloWorld.py

--- A0/GutKat-HelloWorld.py
+++ A0/GutKat-HelloWorld.py
@@ -2,7 +2,7 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description='A0 - Warm up assignment: Hello World')
-parser.add_argument('-i', '--input', type=str, help='the input file')
+parser.add_argument('input', type=str, help='the input file')
 args = parser.parse_args()
 
 with open(args.input, "r") as f:
