228c1c50024808f5c5be15754fe2e4ddbc4822fc
DriftKing1998 <hofer.michael.98@gmail.com>
2023-03-09 17:30:42 +0100

    test

---
+++ A0/A0_Hofer.py
@@ -0,0 +1,11 @@
+import sys
+
+# reading in the file given in the command line
+opened_test = open(sys.argv[1], 'r')
+
+# printing out 'Hello World'
+print(f'Hello World')
+# printing out each line in the input separately
+for line in opened_test:
+    print(line.rstrip())
+
