3691a83d511573ffac125a80d6bca8dd4162f6d6
Julia Gobl <gobl.julia96@gmail.com>
2023-03-13 19:23:33 +0100

    gobjul-A0

--- A0/gojul-HelloWorld.py
+++ A0/gojul-HelloWorld.py
@@ -1,5 +1,10 @@
-inp = ("HelloWorld-test1.in")
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("input", metavar="input", type=str, help="enter your input")
+args = parser.parse_args()
 
-with open (inp, "r") as content:
+input = args.input
+
+with open (input, "r") as content:
     print("Hello World!\n")
     print(content.read().rstrip("\n"))
